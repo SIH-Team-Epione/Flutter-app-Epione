891e6f1123678db5e16671a68d1af3a777b18adf
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2022-07-29 21:45:29 +0530

    UI Improvements.

--- lib/screens/doctor_details_screen.dart
+++ lib/screens/doctor_details_screen.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
-import 'package:quiz_app/constants.dart';
-import 'package:quiz_app/models/Doctors.dart';
 import 'package:maps_launcher/maps_launcher.dart';
+import 'package:quiz_app/models/Doctors.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:quiz_app/models/Quotes.dart';
-import 'package:quiz_app/screens/welcome/welcome_screen.dart';
-import 'package:quiz_app/util/my_button_icon_card.dart';
 
 class DoctorDetailsScreen extends StatefulWidget {
   final int index;
@@ -17,8 +10,7 @@ class DoctorDetailsScreen extends StatefulWidget {
   const DoctorDetailsScreen({Key? key, required this.index}) : super(key: key);
 
   @override
-  State<DoctorDetailsScreen> createState() =>
-      _DoctorDetailsScreenState();
+  State<DoctorDetailsScreen> createState() => _DoctorDetailsScreenState();
 }
 
 class _DoctorDetailsScreenState extends State<DoctorDetailsScreen> {
@@ -29,76 +21,94 @@ class _DoctorDetailsScreenState extends State<DoctorDetailsScreen> {
         title: Text("Doctor Details"),
       ),
       body: Container(
-        color: Colors.black12,
-        child: Container(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('assets/images/user_image.png'),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/user_image.png'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+              child: Text(
+                doctors[widget.index].name,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              Container(
-                margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
-                  child: Text(doctors[widget.index].name)),
-              Container(
-                margin: EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("Hospital"),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                        child: Text("Hospital"),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                        child: Text("Years of exp"),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                        child: Text("Specialisation"),
+                      )
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                        child: Text(
+                          doctors[widget.index].name,
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("Years of exp"),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                        child: Text(
+                          doctors[widget.index].yearsOfExp.toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("Specialisation"),
-                        )
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(doctors[widget.index].name),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                        child: Text(
+                          doctors[widget.index].specialisation,
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(doctors[widget.index].yearsOfExp.toString()),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(doctors[widget.index].specialisation),
-                        )
-                      ],
-                    )
-                  ],
-                ),
+                      )
+                    ],
+                  )
+                ],
               ),
-              Container(
-                color: Colors.deepOrange,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    _buildButtonColumn(Colors.blue, Icons.call, 'Call', 0),
-                    _buildButtonColumn(Colors.blue, Icons.mail, 'Email', 1),
-                    _buildButtonColumn(Colors.blue, Icons.language, 'Website', 2),
-                    _buildButtonColumn(Colors.blue, Icons.location_on, 'Map', 3),
-                  ],
-                ),
-              )
-            ],
-          ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildButtonColumn(Colors.green, Icons.call, 'Call', 0),
+                  _buildButtonColumn(Colors.green, Icons.mail, 'Email', 1),
+                  _buildButtonColumn(Colors.green, Icons.language, 'Website', 2),
+                  _buildButtonColumn(Colors.green, Icons.location_on, 'Map', 3),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -149,6 +159,7 @@ class _DoctorDetailsScreenState extends State<DoctorDetailsScreen> {
           }
       }
     }
+
     return GestureDetector(
       onTap: () {
         itemClickHandler(position);
