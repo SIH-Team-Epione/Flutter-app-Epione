a84c1dde4602696023e907d1edbfd70e0656d180
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2022-08-22 01:32:11 +0530

    added concentration features in work health

---
+++ lib/models/Tips.dart
@@ -0,0 +1,33 @@
+// list of tips to increase concentration
+const List concentration_tips = [
+  {
+    "id" : 0,
+    "imgUrl" : "https://media.istockphoto.com/vectors/business-man-climbing-up-the-stairs-reaching-human-head-to-add-piece-vector-id1031459576?b=1&k=20&m=1031459576&s=612x612&w=0&h=_pDrF5mlfCdcg1pvBSx8xXpYqZ3uRyvJrPOTSpsL_t8=",
+    "title" : "The Brain Games!",
+    "description" : "Playing certain types of games can help you increase your concentration! Try playing Sudoku, chess, crossword puzzles, jigsaw puzzles and many more such games to increase your brain power. Brain training games can also help develop your working and short-term memory, as well as your processing and problem-solving skills."
+  },
+  {
+    "id" : 1,
+    "imgUrl" : "https://img.freepik.com/premium-vector/woman-sleeping-night-with-cat-girl-sleep-bed-pillow-duvet-peaceful-dream-relax-resting-time-comfortable-relaxation-sweet-dreams-good-health_458444-1005.jpg?w=2000",
+    "title" : "Keep Calm and Sleep!",
+    "description" : "Sleep deprivation can easily disrupt concentration, not to mention other cognitive functions, such as memory and attention. Occasional sleep deprivation may not cause too many problems for you. But regularly failing to get a good night’s sleep can affect your mood and performance. Try to get atleast 7-8 hours of sleep each night!"
+  },
+  {
+    "id" : 2,
+    "imgUrl" : "https://img.freepik.com/free-vector/pilates-concept-illustration_114360-1111.jpg?w=2000",
+    "title" : "Work it out!",
+    "description" : "Increased concentration is among the many benefits of regular exercise. a year of moderate aerobic physical activity can help stop or even reverse memory loss that occurs with brain atrophy related to age. So, pull up your socks and get going!"
+  },
+  {
+    "id" : 3,
+    "imgUrl" : "https://img.freepik.com/free-vector/active-tourist-hiking-mountain-man-wearing-backpack-enjoying-trekking-looking-snowcapped-peaks-vector-illustration-nature-wilderness-adventure-travel-concept_74855-9800.jpg?w=2000",
+    "title" : "Nature - The best healer",
+    "description" : "If you want to boost your concentration naturally, try to get outside every day, even for a short while. You might take a short walk through a park. Sitting in your garden or backyard can also help. Natural environments could benefit brain development and may improve attention, so take a stroll out in the wilderness!"
+  },
+  {
+    "id" : 4,
+    "imgUrl" : "https://img.freepik.com/free-vector/organic-flat-people-meditating-illustration_23-2148906557.jpg?w=2000",
+    "title" : "Find comfort in meditation",
+    "description" : "If you want to boost your concentration naturally, try to get outside every day, even for a short while. You might take a short walk through a park. Sitting in your garden or backyard can also help. Natural environments could benefit brain development and may improve attention, so take a stroll out in the wilderness!"
+  }
+];
