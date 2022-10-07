e81cfb77676b144c869318a3d07c61e5c369a41f
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2022-10-07 19:23:00 +0530

    handled issue of multiple names of apps and removed system apps in app_usage

--- lib/screens/mental_health/tempCodeRunnerFile.dart
+++
@@ -1,9 +0,0 @@
-Expanded(
-                                    child: Container(
-                                      margin: EdgeInsets.only(left: 10,right: 10),
-                                      child: Text(
-                                        "Tab1",
-                                        style: TextStyle(color: Colors.black),
-                                      ),
-                                    ),
-                                  ),
