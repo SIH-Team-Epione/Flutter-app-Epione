2b6f8797b9651468c69be7738960f201f5f23b83
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2023-01-09 01:59:18 +0530

    UI changes for video call

--- lib/screens/social_health/join_screen.dart
+++ lib/screens/social_health/join_screen.dart
@@ -14,25 +14,91 @@ class JoinScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-            child: const Text("Create Meeting"),
-            onPressed: onCreateMeetingButtonPressed),
-        const SizedBox(height: 16),
-        TextField(
-            decoration: const InputDecoration(
-              hintText: "Meeting ID",
-              border: OutlineInputBorder(),
+    return Container(
+      color: Colors.grey[200],
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.width * 0.75,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/VideoCall.webp'),
+                fit: BoxFit.fill,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 4,
+                  spreadRadius: 3,
+                  color: Color(0xffd0d0d0),
+                  offset: Offset(3, 3),
+                ),
+              ],
             ),
-            onChanged: onMeetingIdChanged),
-        const SizedBox(height: 8),
-        ElevatedButton(
-          child: const Text("Join"),
-          onPressed: onJoinMeetingButtonPressed,
-        )
-      ],
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          Divider(
+            height: 1,
+            thickness: 2,
+            color: Colors.teal[400],
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              child: const Text(
+                "Create a new video room",
+                style: TextStyle(fontSize: 16),
+              ),
+              onPressed: onCreateMeetingButtonPressed),
+          const SizedBox(height: 16),
+          Text(
+            "OR",
+            style: TextStyle(
+                color: Colors.teal[400],
+                fontSize: 16,
+                fontWeight: FontWeight.w600),
+          ),
+          // const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 8),
+            child: Text(
+              "Join an existing room",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          TextField(
+              decoration: const InputDecoration(
+                hintText: "Room ID",
+                border: OutlineInputBorder(),
+              ),
+              onChanged: onMeetingIdChanged),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 36),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            child: const Text(
+              "Join",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            onPressed: onJoinMeetingButtonPressed,
+          )
+        ],
+      ),
     );
   }
 }
